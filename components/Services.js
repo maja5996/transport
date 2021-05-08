4efcefb099f82cde695ff44c1fac96156fa0b278
Marija Mihajlov <mojemail@gmail.com>
2021-05-08 21:28:47 +0200

    Services Branch

--- components/Services.js
+++ components/Services.js
@@ -3,19 +3,19 @@ import styles from "../styles/Home.module.css";
 const Services = () => {
   return (
     <div className={styles.services}>
-      <div>
+      <div className={styles.service}>
         <h4>Prevoz putnika</h4>
       </div>
 
-      <div>
+      <div className={styles.service}>
         <h4>Prevoz paketa</h4>
       </div>
 
-      <div>
+      <div className={styles.service}>
         <h4>Selidbe</h4>
       </div>
 
-      <div>
+      <div className={styles.service}>
         <h4>Prevoz automobila</h4>
       </div>
     </div>
