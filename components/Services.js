aa1a51a42bc6f9993eaca1763a4714804d045855
Marija Mihajlov <mojemail@gmail.com>
2021-05-12 15:57:53 +0200

    Cities added

--- components/Services.js
+++ components/Services.js
@@ -24,7 +24,7 @@ const Services = () => {
         </div>
         <div className={styles.imagePosition}>
           <Image
-            src="/paketi.jpg"
+            src="/van.jpg"
             width={180}
             height={180}
             className={styles.image}
